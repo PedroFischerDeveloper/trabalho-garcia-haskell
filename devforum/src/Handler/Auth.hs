46f3900239a154012142ce8cdd71899e55b94af8
pedro <pedro@t2s.com.br>
2020-11-21 14:56:34 -0300

    [add] adicionando formulários de auth e registro

--- devforum/src/Handler/Auth.hs
+++ devforum/src/Handler/Auth.hs
@@ -10,9 +10,14 @@ import Import
 
 getAuthR :: Handler Html
 getAuthR = defaultLayout $ do
-    [whamlet|
-        <h1>
-            Login de usuário
-            
+     [whamlet|
+     <div>
+        <h1>Login</h1>
+            <form action="POST">
+                <p>Nome</p>
+                    <input type="text" placeholder="insira seu nome" required/>        
+                        <p>Senha</p>
+                            <input type="password" placeholder="insira sua senha" required/>
+                
     |]
 
--- devforum/src/Handler/Register.hs
+++ devforum/src/Handler/Register.hs
@@ -11,6 +11,17 @@ import Import
 getRegisterR :: Handler Html
 getRegisterR = defaultLayout $ do
     [whamlet|
+        <form action="POST">
+            <p>Nome</p>
+                <input type="text" placeholder="insira seu nome" required/>
+                    <p>Senha</p>
+                        <input type="password" placeholder="insira sua senha" required/>
+            
+    |]
+
+postRegisterR :: Handler Html
+postRegisterR = defaultLayout $ do
+    [whamlet|
         <h1>
             Cadastro de usuário
             
