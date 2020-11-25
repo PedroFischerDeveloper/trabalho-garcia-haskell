8a80bab0b6a2976475ef5551a1d031fbaa7ac123
pedro <pedro@t2s.com.br>
2020-11-24 22:21:46 -0300

    [add]

---
+++ src/Handler/PostNew.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Handler.PostNew where
+
+import Import
+
+postForm :: [(Text, Key Category)] -> Form Post
+postForm categories = renderDivs $ Post
+  <$> areq textField "Título do post" Nothing
+  <*> areq textField "Descrição do post" Nothing
+  <*> areq (selectFieldList categories) "Categoria" Nothing
+
+mapCategories :: [Entity Category] -> [(Text, Key Category)]
+mapCategories [] = []
+mapCategories xs = map (\(Entity eid (Category name _)) -> (name, eid)) xs
+
+getPostNewR :: Handler Html
+getPostNewR = do
+    categories  <- runDB $ selectList [] [] 
+    (widget, _) <- generateFormPost $ postForm $ mapCategories categories
+    defaultLayout $ do
+        [whamlet|
+            <div .container>
+                <div class="page-header">
+                    <h1>Criar novo post
+                <form method=post action=@{PostNewR}>
+                    ^{widget}
+                   <input type=submit .btn.btn-primary.btn-block>
+        |]
+
+  
+
+postPostNewR :: Handler Html
+postPostNewR = do
+    categories  <- runDB $ selectList [] [] 
+    ((result, _), _) <- runFormPost $ postForm $ mapCategories categories
+    case result of
+         FormSuccess newpost -> do 
+             _ <- runDB $ insert400 newpost
+             redirect HomeR -- TODO: Enviar para listagem de posts
+         _ -> redirect AuthR
